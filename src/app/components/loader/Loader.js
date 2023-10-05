4023161601233ab49c9a742367efd70e960893e9
Asael Hernandez <zanduva@gmail.com>
2023-10-05 15:18:42 -0600

    debug2

--- src/app/components/loader/Loader.js
+++ src/app/components/loader/Loader.js
@@ -1,6 +1,6 @@
 import "./Loader.css";
 
-export default function () {
+export default function Loader () {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center bg-black gap-4">
         <h1 style={{fontFamily: "Poppins"}} className="text-white text-center text-5xl">Un momento, estamos cargando la plataforma!</h1>
