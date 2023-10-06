68a98e87e58ef55207f808ae9c2a7d1ea9b2955d
Asael Hernandez <zanduva@gmail.com>
2023-10-06 01:03:54 -0600

    hotfix:image

--- src/app/components/Info/Filters.js
+++ src/app/components/Info/Filters.js
@@ -47,68 +47,70 @@ export default function Filters({
         label="Buscar Curso"
         onChange={(e) => setSearch(e.target.value)}
       />
-      <Sections />
+      
+        <Sections />
+     
 
-      <IconButton
-        aria-label="more"
-        id="long-button"
-        aria-controls={open ? "long-menu" : undefined}
-        aria-expanded={open ? "true" : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <FiFilter size={35} />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          "aria-labelledby": "long-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            width: "20ch",
-          },
-        }}
-      >
-        <MenuItem>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={ascendingOrder}
-                onChange={orderFilter}
-                
-                name="ordenar"
-              />
-            }
-            label="A-Z"
-          />
-        </MenuItem>
+      
+        <IconButton
+          aria-label="more"
+          id="long-button"
+          aria-controls={open ? "long-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <FiFilter size={35} />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            "aria-labelledby": "long-button",
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              width: "20ch",
+            },
+          }}
+        >
+          <MenuItem>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={ascendingOrder}
+                  onChange={orderFilter}
+                  name="ordenar"
+                />
+              }
+              label="A-Z"
+            />
+          </MenuItem>
 
-        <MenuItem>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="completados"
-                onChange={completeFilter}
-                checked={completed}
-              />
-            }
-            label="Completados"
-          />
-        </MenuItem>
+          <MenuItem>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="completados"
+                  onChange={completeFilter}
+                  checked={completed}
+                />
+              }
+              label="Completados"
+            />
+          </MenuItem>
 
-        <MenuItem>
-          <FormControlLabel
-            control={<Checkbox name="todos" />}
-            label="En curso"
-            onChange={notCompleteFilter}
-            checked={notCompleted}
-          />
-        </MenuItem>
-      </Menu>
-    </div>
+          <MenuItem>
+            <FormControlLabel
+              control={<Checkbox name="todos" />}
+              label="En curso"
+              onChange={notCompleteFilter}
+              checked={notCompleted}
+            />
+          </MenuItem>
+        </Menu>
+      </div>
   );
 }
